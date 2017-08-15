eba1f1fd0f21154a18660d1c7f554c4313afa8de
Jirka Kremser <jkremser@redhat.com>
2017-08-15 23:38:52 +0200

    testing if cache helps

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -4,6 +4,7 @@ _results_file="./all-results.perf"
 
 if [ -n "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -f "$_results_file" ]; then
   echo -e "Commenting on PR with the results"
+  #foo
   curl -i -XPOST -H "Authorization: token $GITHUB_TOKEN" \
    -d '{"body":"Results:\n'`cat $_results_file`'\n:balloon: :balloon: :balloon:"}' \
    https://api.github.com/repos/radanalyticsio/workshop-notebook/issues/$TRAVIS_PULL_REQUEST/comments 1>&2
